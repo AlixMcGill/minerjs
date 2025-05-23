ebb047b10ed05342fa85712542900321975f4b69
Alix <alixfmcgill@gmail.com>
2025-05-22 23:51:47 -0500

    Not working login and auth

--- go.mod
+++ go.mod
@@ -1,8 +1,12 @@
 module minerjsgame
 
-go 1.22.2
+go 1.23.0
+
+toolchain go1.23.9
 
 require (
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 )
+
+require golang.org/x/crypto v0.38.0
--- main.go
+++ main.go
@@ -6,11 +6,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
+
 	"github.com/joho/godotenv"
-    _ "github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 var db *sql.DB
+
+type Login struct {
+    HasedPassword string
+    SessionToken string
+    CSRFToken string
+}
+
+// Key is the username
+// convert to database later
+var users = map[string]Login{}
 
 func initDB() {
     err := godotenv.Load()
@@ -46,9 +58,137 @@ func main() {
     fs := http.FileServer(http.Dir("./static"))
     http.Handle("/", fs)
 
+    http.HandleFunc("/register", register)
+    http.HandleFunc("/login", login)
+    http.HandleFunc("/logout", logout)
+    http.HandleFunc("/protected", protected)
+
     fmt.Println("Server started at http://localhost:8080")
     err := http.ListenAndServe(":8080", nil)
     if err != nil {
         panic(err)
     }
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        er := http.StatusMethodNotAllowed
+        http.Error(w, "Invalid method", er)
+        return
+    }
+
+    username := r.FormValue("username")
+    password := r.FormValue("password")
+
+    if len(username) < 8 || len(password) < 8 {
+        er:= http.StatusNotAcceptable
+        http.Error(w, "Invalid username/password", er)
+        return
+    }
+
+    if _, ok := users[username]; ok {
+        er := http.StatusConflict
+        http.Error(w, "User already exists", er)
+        return
+    }
+
+    hashedPassword, _ := hashPassword(password)
+    users[username] = Login{
+        HasedPassword: hashedPassword,
+    }
+
+    fmt.Fprintln(w, "User registered successfully!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        er := http.StatusMethodNotAllowed
+        http.Error(w, "Invalid method", er)
+        return
+    }
+
+    username := r.FormValue("username")
+    password := r.FormValue("password")
+
+    user, ok := users[username]
+    if !ok || !checkPasswordHash(password, user.HasedPassword) {
+        er := http.StatusUnauthorized
+        http.Error(w, "Invalid username or password", er)
+        return
+    }
+
+    sessionToken := generateToken(32)
+    csrfToken := generateToken(32)
+
+    // set session cookie
+    http.SetCookie(w, &http.Cookie{
+        Name: "session_token",
+        Value: sessionToken,
+        Expires: time.Now().Add(24 * time.Hour),
+        HttpOnly: true,
+    })
+
+    // set CSRF token in a cookie
+    http.SetCookie(w, &http.Cookie{
+        Name: "csrf_token",
+        Value: csrfToken,
+        Expires: time.Now().Add(24 * time.Hour),
+        HttpOnly: false,
+    })
+
+    // store token in database
+    user.SessionToken = sessionToken
+    user.CSRFToken = csrfToken
+    users[username] = user
+
+    fmt.Fprintln(w, "Login successful!")
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+    if err := Authorize(r); err != nil {
+        er := http.StatusUnauthorized
+        http.Error(w, "Unauthorized", er)
+        return
+    }
+
+    // clear cookies
+    http.SetCookie(w, &http.Cookie{
+        Name: "session_token",
+        Value: "",
+        Expires: time.Now().Add(24 * time.Hour),
+        HttpOnly: true,
+    })
+    http.SetCookie(w, &http.Cookie{
+        Name: "csrf_token",
+        Value: "",
+        Expires: time.Now().Add(24 * time.Hour),
+        HttpOnly: false,
+    })
+
+    // clear the tokens from the database
+
+    username := r.FormValue("username")
+    user, _ := users[username]
+    user.SessionToken = ""
+    user.CSRFToken = ""
+    users[username] = user
+
+    fmt.Fprintf(w, "Logged out successfully!")
+}
+
+func protected(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        er := http.StatusMethodNotAllowed
+        http.Error(w, "Invalid request method", er)
+        return
+    }
+
+    if err := Authorize(r); err != nil {
+        er := http.StatusUnauthorized
+        http.Error(w, "Unauthorized", er)
+        return
+    }
+
+    username := r.FormValue("username")
+    fmt.Fprintf(w, "CSRF validation successful! Welcome, %s", username)
 }
